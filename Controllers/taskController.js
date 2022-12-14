f8dfcfc9a54b5dd3333e3989e1d39699462b98d4
Setsuna Yukiii <siang-meng@live.com>
2022-12-14 12:32:29 +0800

    add task

--- Controllers/taskController.js
+++ Controllers/taskController.js
@@ -6,32 +6,81 @@ const Task = require("../Models/taskModel");
 // completed --> boolean; task is completed or not (default is false)
 // tags --> tags involved in the controller?
 // addedDate --> date object contains the following:
-    // year
-    // month
-    // day
-    // time
+// year
+// month
+// day
+// time (can ignore)
 // addedIsoDate --> datetime object --> created based on addedDate object
 
 // =============Create=============
 // add to db
-const addTask = async (req, res) => {};
+// dont need to worry about clashing
+const addTask = async (req, res) => {
+    const userId = req.body.userId;
+    if (userId.length != 24) {
+        res.send({
+            success: false,
+            message: "User does not exist",
+        });
+    } else {
+        const getUser = await User.findOne({ _id: userId });
+        if (!getUser) {
+            res.send({
+                success: false,
+                message: "User does not exist",
+            });
+        } else {
+            const { taskName, tags, addedDate } = req.body;
+            const { year, month, day } = addedDate;
+            const newTask = new Task({
+                userId,
+                taskName,
+                tags,
+                addedDate,
+                completed:false,
+                addedIsoDate: new Date(year, month, day),
+            });
+            if (taskName.length > 20) {
+                res.send({
+                    success: false,
+                    message: "Task name cannot exceed 20 characters",
+                });
+            } else {
+                await Task.create(newTask)
+                    .then((result) => {
+                        res.send({
+                            success: true,
+                            message: "Task added",
+                            data: newTask._id,
+                        });
+                    })
+                    .catch((err) => {
+                        res.send({
+                            success: false,
+                            message: err,
+                        });
+                    });
+            }
+        }
+    }
+};
 // =============Read=============
 // get all from db
 const getTasks = async (req, res) => {
     await Task.find()
-    .then((result) => {
-        res.send({
-            success: true,
-            data: result,
+        .then((result) => {
+            res.send({
+                success: true,
+                data: result,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send({
+                success: false,
+                message: err,
+            });
         });
-    })
-    .catch((err) => {
-        console.log(err);
-        res.send({
-            success: false,
-            message: err,
-        });
-    });
 };
 
 // get all of a given user's db
