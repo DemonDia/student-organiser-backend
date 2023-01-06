a67e6ec6de9a6a3b41dc3cdadccd0b9f8450206d
Setsuna Yukiii <siang-meng@live.com>
2023-01-07 00:11:47 +0800

    httpsOnly setting

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -150,7 +150,7 @@ const loginUser = async (req, res) => {
     res.cookie(String(existingUser._id), token, {
         path: "/",
         expires: new Date(Date.now() + 1000 * 30), // 30 seconds
-        httpOnly: true,
+        httpOnly: false,
         sameSite: "lax",
     });
 
