7bcc7a6f181705293c87ce6e3654dea1d0b7b7b9
Setsuna Yukiii <siang-meng@live.com>
2023-01-07 16:05:32 +0800

    .

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
-    // console.log("Verify token \n")
+    console.log("Verify token \n")
     const cookies = req.headers.cookie;
     const rawToken = cookies.split("=")[1];
-    // console.log("verify rawToken\n",rawToken)
-    // console.log("verify rawToken length\n", rawToken.length);
+    console.log("verify rawToken\n",rawToken)
+    console.log("verify rawToken length\n", rawToken.length);
     const token = rawToken.split("; ")[0];
-    // console.log("verify token\n",token)
-    // console.log("verify token length\n", token.length);
+    console.log("verify token\n",token)
+    console.log("verify token length\n", token.length);
     if (!token) {
         return res.status(404).json({ message: "No token found" });
     }
@@ -22,14 +22,14 @@ const verifyToken = (req, res, next) => {
     });
 };
 const refreshToken = (req, res, next) => {
-    // console.log("Refresh token \n")
+    console.log("Refresh token \n")
     const cookies = req.headers.cookie;
     const rawToken = cookies.split("=")[1];
-    // console.log("verify rawToken\n",rawToken)
-    // console.log("verify rawToken length\n", rawToken.length);
+    console.log("verify rawToken\n",rawToken)
+    console.log("verify rawToken length\n", rawToken.length);
     const prevToken = rawToken.split("; ")[0];
-    // console.log("verify token\n",prevToken)
-    // console.log("verify token length\n", prevToken.length);
+    console.log("verify token\n",prevToken)
+    console.log("verify token length\n", prevToken.length);
     if (!prevToken) {
         return res.status(400).json({ message: "Couldn't find token" });
     }
