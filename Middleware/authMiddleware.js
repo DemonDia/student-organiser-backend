c9d7b2b9e5e8ac503c7bec41d506af160501b97e
Setsuna Yukiii <siang-meng@live.com>
2023-01-09 13:49:56 +0800

    .

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -25,53 +25,34 @@ const verifyToken = (req, res, next) => {
     });
 };
 const refreshToken = (req, res, next) => {
-    console.log("Refresh token \n")
-    const cookies = req.headers.cookie;
-    console.log("cookies",cookies)
-    // console.log(req.headers)
-    // const rawToken = cookies.split("=")[1];
-    // console.log("verify rawToken\n",rawToken)
-    // console.log("verify rawToken length\n", rawToken.length);
-    // const prevToken = rawToken.split("; ")[0];
-
-    const prevToken = cookies.split("=")[1];
-    console.log("verify token\n",prevToken)
-    console.log("verify token length\n", prevToken.length);
-    if (!prevToken) {
-        return res.status(400).json({ message: "Couldn't find token" });
+  const cookies = req.headers.cookie;
+  const prevToken = cookies.split("=")[1];
+  if (!prevToken) {
+    return res.status(400).json({ message: "Couldn't find token" });
+  }
+  jwt.verify(String(prevToken), process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      console.log(err);
+      return res.status(403).json({ message: "Authentication failed" });
     }
-    jwt.verify(String(prevToken), process.env.JWT_SECRET, (err, user) => {
-        if (err) {
-            console.log(err);
-            return res.status(403).json({ message: "Authentication failed" });
-        }
-        // res.clearCookie(`${user.id}`);
-        // res.clearCookie(`${user.id}`);
-        // req.cookies[`${user.id}`] = "";
-        res.cookie(String(user.id), prevToken, {
-            path: "/",
-            expires: new Date(0),
-            httpOnly: true,
-            sameSite: "none",
-            secure: true,
-        });
+    res.clearCookie(`${user.id}`);
+    req.cookies[`${user.id}`] = "";
 
-        const newToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-            expiresIn: "60s",
-        });
-
-        res.cookie(String(user.id), newToken, {
-            path: "/",
-            expires: new Date(Date.now() + 1000 * 60), // 60 seconds
-            httpOnly: true,
-            sameSite: "none",
-            secure: true
-        });
-        
-
-        req.id = user.id;
-        next();
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "35s",
     });
+    console.log("Regenerated Token\n", token);
+
+    res.cookie(String(user.id), token, {
+      path: "/",
+      expires: new Date(Date.now() + 1000 * 30), // 30 seconds
+      httpOnly: true,
+      sameSite: "lax",
+    });
+
+    req.id = user.id;
+    next();
+  });
 };
 
 module.exports = { verifyToken, refreshToken };
