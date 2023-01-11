7e87a7cb2269f8bb261f800f79ec0469f20132d0
Setsuna Yukiii <siang-meng@live.com>
2023-01-11 15:59:48 +0800

    Revert "."
    This reverts commit d87e3a6ebbb0eb5a156f409ab69497ecaf00b45e.

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -46,6 +46,7 @@ const refreshToken = (req, res, next) => {
 
         res.cookie(String(user.id), token, {
             path: "/",
+            domain:process.env.DOMAIN,
             expires: new Date(Date.now() + 1000 * 30),
             httpOnly: true,
             sameSite: "none",
