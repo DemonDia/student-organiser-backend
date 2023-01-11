7d82224ee2a55489f502d9415169746767d1a7b7
Setsuna Yukiii <siang-meng@live.com>
2023-01-11 23:10:25 +0800

    check header

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
     console.log("Verify token \n");
+    console.log("header:",req.headers)
     const cookies = req.headers.cookie;
     console.log("cookies", cookies);
     // console.log(req.headers)
